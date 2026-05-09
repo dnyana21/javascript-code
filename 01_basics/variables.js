990fd573c697f380a691234972121afa60b3bec2
Dnyaneshwar khachane <dnyaneshwarkhachane21@gmail.com>
2026-05-09 16:32:10 +0000

    devcontainer changes and one nwe file added

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 12323
+let accountEmail = "hitesh@google.com"
+var accountPassword = 1234
+accountCity = "Jaipur"
+
+/*
+prefer noto to use var 
+because variable scope and function scope
+*/
